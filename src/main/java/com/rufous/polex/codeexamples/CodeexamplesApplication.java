fc9af2ec890663d60b5986267caee726e50d72b8
Alex <alex@rufous-mac.local>
2025-01-14 13:34:38 +0200

    Init Spring project

---
+++ src/main/java/com/rufous/polex/codeexamples/CodeexamplesApplication.java
@@ -0,0 +1,13 @@
+package com.rufous.polex.codeexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeexamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeexamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rufous/polex/codeexamples/CodeexamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rufous.polex.codeexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeexamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
